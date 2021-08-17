83d6b0d712f7ac6ed9fa004c473d6d3393f99dd4
Joao Vitor Oliveira <jv.oliveraraujo@gmail.com>
2021-08-17 13:51:06 -0300

    Serializer with Gson :beers:

--- src/main/java/org/example/ecommerce/KafkaService.java
+++ src/main/java/org/example/ecommerce/KafkaService.java
@@ -9,16 +9,27 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 public class KafkaService implements Closeable {
     private final KafkaConsumer<String, String> consumer;
     private final ConsumerFunction parse;
 
     public KafkaService(String groupId, String topic, ConsumerFunction parse) {
-        this.parse = parse;
-        this.consumer = new KafkaConsumer<>(properties(groupId));
+        this(parse, groupId);
         consumer.subscribe(Collections.singletonList(topic));
     }
+
+    public KafkaService(String groupId, Pattern topic, ConsumerFunction parse) {
+        this(parse, groupId);
+        consumer.subscribe(topic);
+    }
+
+    private KafkaService(ConsumerFunction parse, String groupId) {
+        this.parse = parse;
+        this.consumer = new KafkaConsumer<>(properties(groupId));
+    }
+
 
     public void run() {
         while(true){
--- src/main/java/org/example/ecommerce/LogService.java
+++ src/main/java/org/example/ecommerce/LogService.java
@@ -1,5 +1,6 @@
 package org.example.ecommerce;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import java.time.Duration;
@@ -9,22 +10,22 @@ import java.util.regex.Pattern;
 public class LogService {
 
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Pattern.compile("ECOMMERCE.*"));
-        while(true){
-            var records = consumer.poll(Duration.ofMillis(100));
-            if(!records.isEmpty()) {
-                System.out.println("Found " + records.count() + " registers");
-                for (var record : records) {
-                    System.out.println("________________________________________");
-                    System.out.println("LOG ::: " + record.topic());
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-                }
-            }
+        var logService = new LogService();
+        try(var service = new KafkaService(
+                LogService.class.getSimpleName(),
+                Pattern.compile("ECOMMERCE.*"),
+                logService::parse)){
+            service.run();
         }
+    }
+
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("________________________________________");
+        System.out.println("LOG ::: " + record.topic());
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
     }
 
     private static Properties properties() {
